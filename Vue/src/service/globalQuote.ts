bdccb1ceae056e22c47fc9ad8afeebf62dd69d2b
BelovedYaoo <belovedyaoo@aliyun.com>
2024-08-14 19:08:29 +0800

    重构(Vue): 补全全局引用模块的注释

--- Vue/src/service/globalQuote.ts
+++ Vue/src/service/globalQuote.ts
@@ -1,6 +1,10 @@
 import {AxiosResponse} from 'axios';
 import {ToastMessageOptions} from 'primevue/toast';
 
+/**
+ * 将Axios请求响应体转换为PrimeVue的Toast配置
+ * @param response Axios请求响应体
+ */
 export const responseToastConfig = (response: AxiosResponse<any>): ToastMessageOptions => {
   return {
     severity: response.data.code === 200 ? 'success' : 'error',
@@ -10,22 +14,36 @@ export const responseToastConfig = (response: AxiosResponse<any>): ToastMessageO
   };
 };
 
+/**
+ * 将类名添加到指定id的元素上，并设置延时移除
+ * @param id 需要添加的元素id名
+ * @param className 被添加的类名
+ * @param second 延时时长(默认5秒延时)
+ */
 export const addClassById = (id: string, className: string, second: number = 5): void => {
   const element: HTMLElement | null = document.getElementById(id);
   if (element) {
     element.classList.add(className);
     setTimeout((): void => {
       element.classList.remove(className);
-    }, second * 1000); // 默认延时5秒
+    }, second * 1000);
   }
 };
 
+/**
+ * 全局配置接口
+ */
 export interface AppConfig {
   appName: string;
   appTokenName: string;
 }
 
+/**
+ * 全局配置
+ */
 export const globalConfig: AppConfig = {
+  // 应用名称
   appName: 'OpenIAM',
+  // Token名称
   appTokenName: 'openToken'
 };
