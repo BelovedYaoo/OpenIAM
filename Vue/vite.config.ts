bfef73ed3a9d1f062dfa68dbb26f05084b6d37d0
BelovedYaoo <belovedyaoo@aliyun.com>
2024-09-20 18:37:16 +0800

    构建(Vue): 新增Code Inspector，快速在IDE内定位元素
    详情参考官网：https://inspector.fe-dev.cn/

--- Vue/vite.config.ts
+++ Vue/vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import {resolve} from 'path';
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -46,7 +47,12 @@ export default defineConfig(() => {
                 }
             }
         },
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            codeInspectorPlugin({
+                bundler: 'vite',
+            }),
+        ],
         resolve: {
             alias: {
                 '@': resolve(__dirname, './src'),
