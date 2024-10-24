4753162009405220ebbe42116cb359b11bac21a4
BelovedYaoo <belovedyaoo@aliyun.com>
2024-10-24 18:08:47 +0800

    重构(Vue): 完善TypeScript的类型支持
    - 在 tsconfig.json 中添加对 "src/**/*.ts" 文件的包含
    - 在 vite-env.d.ts 中声明模块定义，以支持 TypeScript

--- Vue/src/vite-env.d.ts
+++ Vue/src/vite-env.d.ts
@@ -3,3 +3,13 @@ declare module '*.vue' {
     const Component: ReturnType<typeof defineComponent>;
     export default Component;
 }
+
+declare module '@/service/request' {
+    export default function request(config: any): Promise<any>;
+}
+
+declare module 'js-cookie' {
+    export function set(key: string, value: string): void;
+    export function get(key: string): string;
+    export function remove(key: string): void;
+}
