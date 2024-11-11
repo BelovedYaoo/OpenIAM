10ed1f15561e1ed2179bb0cfe70f31bbfebe1246
BelovedYaoo <belovedyaoo@aliyun.com>
2024-11-11 22:45:50 +0800

    重构(Java): 更新项目启动类以支持自动注入 AGCore 中的关键模块
    - 在 SpringBootApplication 注解中添加 scanBasePackages 属性，指定基础模块的包名
    - 添加 MapperScan 注解，配置 MyBatis 的 Mapper 扫描路径
    - 优化了代码注释，解释了包名扫描的必要性

--- Java/src/main/java/top/belovedyaoo/openiam/OpenIAMApplication.java
+++ Java/src/main/java/top/belovedyaoo/openiam/OpenIAMApplication.java
@@ -1,17 +1,21 @@
 package top.belovedyaoo.openiam;
 
 import com.tangzc.autotable.springboot.EnableAutoTable;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 启动类
+ * 启动类<br>
+ * 因AGCore中存在系统关键的模块，如基础ID处理器，所以需要将基础模块的包名加入扫描路径中<br>
+ * 因本项目包名与AGCore包名的一、二级包名都相同，故此处直接扫描二级包名
  *
  * @author BelovedYaoo
  * @version 1.0
  */
 @EnableAutoTable
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages =  {"top.belovedyaoo"})
+@MapperScan(basePackages = "top.belovedyaoo")
 public class OpenIAMApplication {
 
     public static void main(String[] args) {
